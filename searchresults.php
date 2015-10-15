189218942a081bfaf28a69bd2f23b5fc712c2b2b
Yasir Taher <ad-bd01@Yasir.local>
2015-10-15 17:39:09 +0600

    151015

--- searchresults.php
+++ searchresults.php
@@ -24,12 +24,20 @@
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/titan.css" type="text/css">
+    <link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+      <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+      <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 
-
-
+    <script>
+    $('#flat-slider').slider({
+        orientation: 'horizontal',
+        range:       true,
+        values:      [17,67]
+    });
+    </script>
 </head>
 
-<body id="page-top" style="background-color:#111">
+<body id="page-top">
 
     <nav id="resultheader" class="navbar navbar-default navbar-static-top">
         <div class="container">
@@ -66,79 +74,167 @@
         <!-- /.container -->
     </nav>
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="searchcriteria col-md-12">
-            <p>SEARCH FILTER</p>
+            <div class="searchcriteria col-md-12" style="height:100px">
+                
+                      <div class="col-md-2">
+                          <span>Budget <br></span>
+                          <div class="flat-slider" id="flat-slider"></div>
+                         
+                      </div>
+
+                      <div class="col-md-2" style="background-color:#fff;height:10px">
+                          <span>Furnished <br><i class="fa fa-search"></i><i class="fa fa-search"></i></span>
+                      </div>
+                      <div class="col-md-2">
+                          <span>Live with <br><i class="fa fa-user"></i>
+                          <i class="fa fa-user"></i>
+                          </span>
+                      </div>  
+                      
+                      <div class="col-md-2">
+                        <span>n roommates  <br>
+                            <select>
+                              <option value="volvo">Volvo</option>
+                              <option value="saab">Saab</option>
+                              <option value="mercedes">Mercedes</option>
+                              <option value="audi">Audi</option>
+                            </select> 
+                        </span>
+                      </div> 
+
+                      <div class="col-md-2">
+                            <span>They should <form action="">
+                                <input type="radio" name="occupation" value="study"> Study
+                                <input type="radio" name="occupation" value="work"> Work
+                                </form>
+                            </span>
+                      </div> 
+
+                      <div class="col-md-1">
+                          <span>Pet-Friendly <i class="fa fa-search"></i><i class="fa fa-search"></i></span>
+                      </div> 
+
+                      <div class="col-md-1">
+                         <span>New Search <i class="fa fa-search"></i></span>
+                      </div> 
+              
+
+                <div class="col-md-6">
+                    
+                </div>                
             </div>
 
             <div class="mainView col-md-12">
                 <div class="searchMap col-md-8 col-xs-12">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d366.31072781234434!2d90.41294069744393!3d23.813511352687115!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1444811049438" width="100%" height="550" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <!-- <iframe style="border: 1px solid rgb(142, 137, 131);" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d366.31072781234434!2d90.41294069744393!3d23.813511352687115!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1444811049438" width="100%" height="550" frameborder="0" style="border:0" allowfullscreen></iframe> -->
                 </div>
-
-                <div class="col-md-4 col-xs-12">
-      <!--              <div class="container">
-                        <div class="row">
-
-                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"> -->
-                                <div class="offer offer-success">
-                                    <div class="user-avatar">
-                                        <img class="avatar-text" src="img/user.jpg">                            
-                                    </div>
-
-                                    <div class="shape">
-                                        <div class="shape-text">
-                                            <i class="fa fa-heart fa-2x"></i>                            
-                                        </div>
-                                    </div>
-
-                                     <div class="user-icon">
-                                        <div class="user-icon-text">
-                                            <i class="fa fa-user fa-2x"></i>                            
-                                        </div>
-                                    </div>
-
-                                    <div class="offer-content">
-                                        <h3 class="lead">
-                                            A success offer
-                                        </h3>                       
-                                        <p><br> and so one</p>
-                                    </div>
-
-                                </div>
-
-                            <!-- </div> -->
-
-                  <!--       </div>
-                    </div> -->
-                    
-
-                   <!--  <div class="propertyList">
-                    
-                    <div class="row">
-                        <div class="agentAvatar col-md-2"></div>
-                        <div class="pull-right col-md-2"><i style="margin-top:8px" class="fa fa-heart fa-2x"></i></div>
-                    </div> -->
-                    
-                    <!-- <div class="row">
-                        <div class="col-md-2" >
-                            <i class="fa fa-user fa-2x" style="margin-top: 160px; margin-left: -50px;"></i>
-                            <i class="fa fa-user fa-2x" ></i>
+                 <!-- START PROPERTY LIST -->
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="offer">
+                        <div class="user-avatar">
+                            <img class="avatar-text" src="img/user.jpg">                            
                         </div>
-                        <div class="col-md-2"><i class="fa fa-map-marker fa-2x" style="margin-top: 160px;"></i> <p>DC</p></div>
-                    </div> -->
+
+                        <div class="shape">
+                            <div class="shape-text">
+                                <i class="fa fa-heart fa-2x"></i>                            
+                            </div>
+                        </div>
+
+                         <div class="user-icon">
+                            <div class="user-icon-text">
+                                <span> 1 <i class="fa fa-user fa-2x"></i></span> 
+                                <span>1 <i class="fa fa-user fa-2x"></i></span>                          
+                            </div>
+                        </div>
+
+                         <div class="rent-icon">
+                            <div class="rent-text">
+                                <span><i class="fa fa-usd "></i>1000</span>                                                       
+                            </div>
+                        </div>
+
+                        <div class="place-icon">
+                            <div class="place-text">
+                                <span><i class="fa fa-map-marker "></i> Washington DC</span> 
+                                                       
+                            </div>
+                        </div>
                     </div>
-                    
-                    <!-- <div class="propertyList">
-                        <div class="agentAvatar"></div>
-                    </div> -->
-                <!--     <img class="propertyList" src="img/property2.jpg">
-                    <img class="propertyList" src="img/property3.jpg">
-                    <img class="propertyList" src="img/property1.jpg">  -->
+                    <!-- property -->
+                    <div class="offer">
+                        <div class="user-avatar">
+                            <img class="avatar-text" src="img/user.jpg">                            
+                        </div>
+
+                        <div class="shape">
+                            <div class="shape-text">
+                                <i class="fa fa-heart fa-2x"></i>                            
+                            </div>
+                        </div>
+
+                         <div class="user-icon">
+                            <div class="user-icon-text">
+                                <span>1 <i class="fa fa-user fa-2x"></i></span> 
+                                <span>1 <i class="fa fa-user fa-2x"></i></span>                          
+                            </div>
+                        </div>
+
+                         <div class="rent-icon">
+                            <div class="rent-text">
+                                <span><i class="fa fa-usd "></i>1000</span> 
+                                                       
+                            </div>
+                        </div>
+
+                        <div class="place-icon">
+                            <div class="place-text">
+                                <span><i class="fa fa-map-marker "></i> Washington DC</span> 
+                                                       
+                            </div>
+                        </div>
+                    </div>
+ <!-- property -->
+                    <div class="offer">
+                        <div class="user-avatar">
+                            <img class="avatar-text" src="img/user.jpg">                            
+                        </div>
+
+                        <div class="shape">
+                            <div class="shape-text">
+                                <i class="fa fa-heart fa-2x"></i>                            
+                            </div>
+                        </div>
+
+                         <div class="user-icon">
+                            <div class="user-icon-text">
+                                <span>1 <i class="fa fa-user fa-2x"></i></span> 
+                                <span>1 <i class="fa fa-user fa-2x"></i></span>                          
+                            </div>
+                        </div>
+
+                         <div class="rent-icon">
+                            <div class="rent-text">
+                                <span><i class="fa fa-usd "></i>1000</span>                                                        
+                            </div>
+                        </div>
+
+                        <div class="place-icon">
+                            <div class="place-text">
+                                <span><i class="fa fa-map-marker "></i> Washington DC</span>                                                        
+                            </div>
+                        </div>
+                    </div>
+                    <!-- property -->
+
                 </div>
 
+                <!-- END PROPERTY LIST -->
             </div>
+
+        </div>
 
 
         </div>
