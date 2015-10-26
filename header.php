3f1369e4d834854afb8afdf291f0a5c68ad1a17f
Yasir Taher <ad-bd01@Yasir.local>
2015-10-26 11:55:48 +0600

    Roommates Result Updated

--- header.php
+++ header.php
@@ -39,7 +39,9 @@
     <script type="text/javascript" src="js/jquery.dependClass-0.1.js"></script>
     <script type="text/javascript" src="js/draggable-0.1.js"></script>
     <script type="text/javascript" src="js/jquery.slider.js"></script>
-
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </head>
 
--- result_roommates.php
+++ result_roommates.php
@@ -61,60 +61,122 @@
             <!-- END SEARCH CRITERIA -->
 
             <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="col-md-2"></div>
-            <div class="col-md-8 col-sm-8 col-xs-12">
-            <div class="roommates">
-                <div class="col-md-3 col-sm-3 col-xs-3 roommate_image"><img src="img/user.jpg"></div>
-                <div class="col-md-6 col-sm-6 col-xs-6">
+            <div class="col-md-2 col-sm-1"></div>
+            <div class="col-md-8 col-sm-10 col-xs-12">
+            
+           <div class="roommates">
+                <div class="col-md-3 col-sm-3 col-xs-12 roommate_image"><img src="img/user.jpg"></div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
                     <span><h2>Lee R. Parker </h2></span>
                     <span><h4><i class="fa fa-map-marker"> Washington, D.C</i></h4></span>
                     <span><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                     and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></span>
                     <span>Searching for</span>
                 </div>
-                <div class="roommate_rating col-md-3 col-sm-3 col-xs-3">
+                <div class="roommate_rating col-md-3 col-sm-3 col-xs-12">
+                <div class="ratingtext">
                     <span class="rat"><i class="fa fa-star fa-2x"></i> 4,2/5</span>
                     <h4>I have credit card</h4>
-                    <button class="btn btn-chat-roommate"><i class="fa fa-comment-o fa-2x"></i> sent me a message</button>
+                      <button data-toggle="modal" data-target="#messageModal" class="btn btn-chat-roommate"><i class="fa fa-comment-o fa-2x"></i> sent me a message</button>
+                </div>
                 </div>
             </div>
 
              <div class="roommates">
-                <div class="col-md-3 col-sm-3 col-xs-3 roommate_image"><img src="img/user.jpg"></div>
-                <div class="col-md-6 col-sm-6 col-xs-6">
-                    <span><h2>Floyd Joiner </h2></span>
+                <div class="col-md-3 col-sm-3 col-xs-12 roommate_image"><img src="img/user.jpg"></div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <span><h2>Lee R. Parker </h2></span>
                     <span><h4><i class="fa fa-map-marker"> Washington, D.C</i></h4></span>
                     <span><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                     and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></span>
                     <span>Searching for</span>
                 </div>
-                <div class="roommate_rating col-md-3 col-sm-3 col-xs-3">
-                    <span class="rat"><i class="fa fa-star fa-2x"></i> 4/5</span>
+                <div class="roommate_rating col-md-3 col-sm-3 col-xs-12">
+                <div class="ratingtext">
+                    <span class="rat"><i class="fa fa-star fa-2x"></i> 4,2/5</span>
                     <h4>I have credit card</h4>
-                    <button class="btn btn-chat-roommate"><i class="fa fa-comment-o fa-2x"></i> sent me a message</button>
+                      <button data-toggle="modal" data-target="#messageModal" class="btn btn-chat-roommate"><i class="fa fa-comment-o fa-2x"></i> sent me a message</button>
+                </div>
                 </div>
             </div>
 
-              <div class="roommates">
-                  <div class="col-md-3 col-sm-3 col-xs-3 roommate_image"><img src="img/user.jpg"></div>
-                  <div class="col-md-6 col-sm-6 col-xs-6">
-                      <span><h2>Telma G. Biggs </h2></span>
-                      <span><h4><i class="fa fa-map-marker"> Washington, D.C</i></h4></span>
-                      <span><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                      and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></span>
-                      <span>Searching for</span>
-                  </div>
-                  <div class="roommate_rating col-md-3 col-sm-3 col-xs-3">
-                      <span class="rat"><i class="fa fa-star fa-2x"></i> 4,5/5</span>
-                      <h4>I have credit card</h4>
-                      <button class="btn btn-chat-roommate"><i class="fa fa-comment-o fa-2x"></i> sent me a message</button>
-                  </div>
+            <div class="roommates">
+                <div class="col-md-3 col-sm-3 col-xs-12 roommate_image"><img src="img/user.jpg"></div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <span><h2>Lee R. Parker </h2></span>
+                    <span><h4><i class="fa fa-map-marker"> Washington, D.C</i></h4></span>
+                    <span><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></span>
+                    <span>Searching for</span>
+                </div>
+                <div class="roommate_rating col-md-3 col-sm-3 col-xs-12">
+                <div class="ratingtext">
+                    <span class="rat"><i class="fa fa-star fa-2x"></i> 4,2/5</span>
+                    <h4>I have credit card</h4>
+                      <button data-toggle="modal" data-target="#messageModal" class="btn btn-chat-roommate"><i class="fa fa-comment-o fa-2x"></i> sent me a message</button>
+                </div>
+                </div>
+            </div>
+<button data-toggle="modal" data-target="#confirmationModal" class="btn btn-chat-roommate"> another modal</button>
+            </div>
+
+             <div class="col-md-2 col-sm-1"></div>              
             </div>
 
 
-            </div>
+<!-- line modal -->
+      <div class="modal fade" id="messageModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+            <h3 class="modal-title" id="lineModalLabel"></h3>
+          </div>
+          
+          <div class="modal-body">
+            <!-- content goes here -->
+            <form accept-charset="UTF-8" action="" method="POST">
+            <h3>Sent a message to "Name"</h3>
+              <textarea class="form-control" id="message" name="message" placeholder="Do you think i can be your roommate? What do you want to know about me?" rows="8" style="margin-bottom:10px;"></textarea>
+                <button class="btn btn-chat-roommate pull-right" type="submit"> <i class="fa fa-comment-o fa-2x"></i> Message "NAME"</button>
+                
 
-             <div class="col-md-2"></div>
+            </form>
 
-              
+          </div>
+    <div class="modal-footer">
+      <div class="btn-group btn-group-justified" role="group" aria-label="group button">
+
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+
+
+<!-- line modal -->
+      <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+            <h3 class="modal-title" id="lineModalLabel"></h3>
+          </div>
+          
+          <div class="modal-body">
+            <div class="confirmationMessage">
+              <h3>Your Message has been sent!</h3><br>
+              <p>We will notify you when Name replies your message</p><br>
+
+              <a href="#" type="button" class="btn btn-chat-roommate">Show me more roommates</a>
             </div>
+          </div>
+    <div class="modal-footer">
+      <div class="btn-group btn-group-justified" role="group" aria-label="group button">
+
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+
