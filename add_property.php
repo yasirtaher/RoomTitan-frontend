164b9d8e65b991dedbc78ec2c4c8e7f1e955b32a
Yasir Taher <ad-bd01@Yasir.local>
2015-10-27 12:51:18 +0600

    Profit and Loss, Add Room

---
+++ add_property.php
@@ -0,0 +1,172 @@
+<?php include('header.php') ?>
+<div class="container">
+  	<div class="row shadow_with_cross add_room">
+  		<form class="">
+  		<div class="col-md-5">
+  			
+  			<h4>ROOM DETAILS</h4>
+			<div class="form-group">
+			    <input type="text" class="form-control customfield" id="address" placeholder="Major intersection">
+			</div>
+			
+			<div class="form-group">
+			    <select class="customfield form-control">
+	              	<option value="" selected>Select state</option>
+	              	<option value="">Room</option>
+	              	<option value="">Parking</option>
+	          	</select>
+			</div>
+			
+			<div class="form-group">
+			    <select class="customfield form-control">
+	            	<option value="" selected>Select city</option>
+	             	<option value="">Room</option>
+	            	<option value="">Parking</option>
+	          	</select>
+			</div>
+			
+			<div class="form-group">
+			    <input type="number" class="form-control customfield" id="zipcode" placeholder="Zip code">
+			</div>
+			  
+		  	<div class="row">
+				<div class="col-md-4"><label>Existing occupants</label></div>
+			    <div class="col-md-8">
+			    	<div class="form-group">
+			    		<select class="customfield form-control" placeholder="Major intersection">
+	              			<option value="">Listing Type</option>
+	              			<option value="">Room</option>
+	              			<option value="">Parking</option>
+	          			</select>
+	          		</div>
+			  	</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4"><label>Occupation</label></div>
+			    <div class="col-md-8">
+			    	<div class="form-group">
+			    		<select class="customfield form-control" placeholder="Major intersection">
+	              			<option value="" selected>Select occupation</option>
+	              			<option value="">Work</option>
+	              			<option value="">Study</option>
+	          			</select>
+	          		</div>
+			  	</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6"><label>Who are you looking to rent you property?</label></div>
+			    <div class="col-md-6">
+			    	<input type="radio" name="gender" value="" checked> Male
+					<input type="radio" name="gender" value=""> Female
+					<input type="radio" name="gender" value=""> Both
+			  	</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6"><label>Credit check required</label></div>
+			    <div class="col-md-6">
+			    	<input type="radio" name="credit" value="" checked> yes
+					<input type="radio" name="credit" value=""> no
+			  	</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6"><label>Pet-friendly</label></div>
+			    <div class="col-md-6">
+			    	<input type="radio" name="pet" value="" checked> yes
+					<input type="radio" name="pet" value=""> no
+			  	</div>
+			</div>
+
+			<div class="form-group">
+				<input type="number" class="form-control customfield" id="address" placeholder="$ Rent">
+			</div>
+			
+			<div class="row">
+				<div class="col-md-4"><label>Utilities included:</label></div>
+			    <div class="col-md-8">
+			    	<input type="radio" name="utilities" value="" checked> Water
+					<input type="radio" name="utilities" value=""> Electricity
+					<input type="radio" name="utilities" value=""> Wifi
+					<input type="radio" name="utilities" value=""> none
+			  	</div>
+			</div>
+
+			
+
+			  <div class="form-group">
+			    <input type="number" class="form-control customfield" id="address" placeholder="$ Deposit">
+			  </div>
+
+			<div class="row">
+				<div class="col-md-6"><label>Furnished</label></div>
+			    <div class="col-md-6">
+			    	<input type="radio" name="furnished" value="" checked> yes
+					<input type="radio" name="furnished" value=""> no
+			  	</div>
+			</div>
+
+
+		  
+  		</div>
+  		<div class="col-md-1"></div>
+  		<div class="col-md-6">
+  			<h4>MORE INFORMATION</h4>
+  			<div class="form-group">
+				<textarea class="form-control customfield"placeholder="Description" rows="5"></textarea>
+			</div>
+			<div class="form-group">
+				<textarea class="form-control customfield"placeholder="Any special rule?" rows="3"></textarea>
+			</div>
+
+
+			<h4>PHOTOS</h4>
+			<div class="col-md-4 image-upload">
+	    		<label for="file-input">
+	        		<img src="img/front.png" style="width: 100%"/>
+	    		</label>
+	    		<input id="file-input" type="file"/>
+			</div>
+
+			<div class="col-md-4 image-upload">
+	    		<label for="file-input">
+	        		<img src="img/room.png" style="width: 100%"/>
+	    		</label>
+	    		<input id="file-input" type="file"/>
+			</div>
+
+			<div class="col-md-4 image-upload">
+	    		<label for="file-input">
+	        		<img src="img/bathroom.png" style="width: 100%"/>
+	    		</label>
+	    		<input id="file-input" type="file"/>
+			</div>
+			<div class="col-md-4 image-upload">
+	    		<label for="file-input">
+	        		<img src="img/kitchen.png" style="width: 100%"/>
+	    		</label>
+	    		<input id="file-input" type="file"/>
+			</div>
+
+			<div class="col-md-4 image-upload">
+	    		<label for="file-input">
+	        		<img src="img/livingroom.png" style="width: 100%"/>
+	    		</label>
+	    		<input id="file-input" type="file"/>
+			</div>
+
+			<div class="col-md-4 image-upload">
+	    		<label for="file-input">
+	        		<img src="img/more.png" style="width: 100%"/>
+	    		</label>
+	    		<input id="file-input" type="file"/>
+			</div>
+			<br>
+  			<button style="width:100%" type="submit" class="btn btn-chat">Submit</button>
+  		</div>
+  		</form>
+	</div>
+</div>
+
--- profit_and_loss.php
+++ profit_and_loss.php
@@ -7,9 +7,9 @@
   		<div class="col-md-12 profitinfo">
   			<table>
   				<tr>
-  					<th>INCOME <small>edit</small></th>
-  					<th>2014</th>
-  					<th>2015</th>
+  					<td><h3><i class="glyphicon glyphicon-plus-sign"></i> INCOME <small class="edittag">edit</small></h3></td>
+  					<th class="middle">2014</th>
+  					<th class="middle">2015</th>
   					<th id="noborder"></th>
   					<th id="noborder"> </th>
   					<th id="noborder"></th>
@@ -17,54 +17,69 @@
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Rent received</td>
-  					<td id="noborderbottom">18,000</td>
-  					<td id="noborderbottom">15,000</td>
+  					<td id="noborderbottom" class="middle">18,000</td>
+  					<td id="noborderbottom" class="middle">15,000</td>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Sales income net</td>
-  					<td id="noborderbottom">18,000</td>
-  					<td id="noborderbottom">15,000</td>
+  					<td id="noborderbottom" class="middle">18,000</td>
+  					<td id="noborderbottom" class="middle">15,000</td>
   				</tr>
   				<tr>
-  					<th>Esx</th>
+  					<td> <h3> <i class="glyphicon glyphicon-plus-sign"></i> EXPENSES <small class="edittag">edit</small></h3></td>
   					<th></th>
   					<th></th>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Counc. Tax</td>
-  					<td id="noborderbottom">-</td>
-  					<td id="noborderbottom">-</td>
+  					<td id="noborderbottom" class="middle">-</td>
+  					<td id="noborderbottom" class="middle">-</td>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Insurance</td>
-  					<td id="noborderbottom">300</td>
-  					<td id="noborderbottom">300</td>
+  					<td id="noborderbottom" class="middle">300</td>
+  					<td id="noborderbottom" class="middle">300</td>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Bills</td>
-  					<td id="noborderbottom">200</td>
-  					<td id="noborderbottom">175</td>
+  					<td id="noborderbottom" class="middle">200</td>
+  					<td id="noborderbottom" class="middle">175</td>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Legal & Adminstrative Fees</td>
-  					<td id="noborderbottom">200</td>
-  					<td id="noborderbottom">175</td>
+  					<td id="noborderbottom" class="middle">200</td>
+  					<td id="noborderbottom" class="middle">175</td>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Management Fees</td>
-  					<td id="noborderbottom">-</td>
-  					<td id="noborderbottom">-</td>
+  					<td id="noborderbottom" class="middle">-</td>
+  					<td id="noborderbottom" class="middle">-</td>
   				</tr>
   				<tr id="noborderbottom">
   					<td id="noborderbottom">Renewal & Maintenance</td>
-  					<td id="noborderbottom">400</td>
-  					<td id="noborderbottom">800</td>
+  					<td id="noborderbottom" class="middle">400</td>
+  					<td id="noborderbottom" class="middle">800</td>
   				</tr>
-  				<tr id="noborderbottom">
+  				<tr id="noborderbottom" class="last">
   					<td id="noborderbottom">Bank Charges</td>
-  					<td id="noborderbottom">75</td>
-  					<td id="noborderbottom">60</td>
+  					<td id="noborderbottom" class="middle">75</td>
+  					<td id="noborderbottom" class="middle">60</td>
   				</tr>
+          <tr class="blank_row" id="noborderbottom">
+            <td id="noborderbottom"></td>
+            <td id="noborderbottom"></td>
+            <td id="noborderbottom"></td>
+          </tr>
+          <tr class="total">
+            <td ><h3>TOTAL</h3></td>
+            <td class="middle"> 16,875</td>
+            <td class="middle"> 13,565</td>
+          </tr>
+          <tr class="blank_row" id="noborderbottom">
+            <td id="noborderbottom"></td>
+            <td id="noborderbottom"></td>
+            <td id="noborderbottom"></td>
+          </tr>
   			</table>
   			
   		</div>
